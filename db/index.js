7222f574f2db4279ba8adbc5e7ef0d7659ea9924
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-20 13:24:22 -0400

    update index with show, add, update commands

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ class DB {
     constructor(connection) {
         this.connection = connection;
     }
+    // SHOW
     allEmployees() {
         return this.connection.promise().query(
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id=role.id LEFT JOIN department on role.department_id=department.id LEFT JOIN employee manager on manager.id = employee.manager_id; "
@@ -11,7 +12,35 @@ class DB {
         );
     }
     allRoles() {
+        return this.connection.promise().query(
+            "SELECT role.id, role.title, department.name AS depart, role.salary FROM role LEFT JOIN department on role.department_id=department.id;"
+        );
 
+    }
+    allDepartments() {
+        return this.connection.promise().query(
+            "SELECT department.id, department.name FROM department;"
+        );
+    }
+    // ADD
+    addEmployee(employee) {
+        return this.connection.promise().query(
+            "INSERT INTO employee SET ?", employee);
+    }
+    addEmployee(role) {
+        return this.connection.promise().query(
+            "INSERT INTO role SET ?", role);
+    }
+    addEmployee(department) {
+        return this.connection.promise().query(
+            "INSERT INTO department SET ?", department);
+    }
+    // UPDATE
+    updateEmployee(employeeId, roleId) {
+        return this.connection.promise().query(
+            "UPDATE employee SET role_id = ? WHERE id = ?",
+            [roleId, employeeId]
+        );
     }
 }
 
